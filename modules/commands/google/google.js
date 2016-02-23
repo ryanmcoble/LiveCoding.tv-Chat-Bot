c73224e1d39cfef707498b856fe65546534f5193
Ryan Coble <ryan@ryans-mbp.attlocal.net>
2016-02-22 23:55:46 -0500

    Fixed google command so it doesn't show null results, shows only three but queries for me we dont get null results.

--- modules/commands/google/google.js
+++ modules/commands/google/google.js
@@ -2,7 +2,9 @@ module.exports = function() {
 
 	var google = require('google');
 
-	google.resultsPerPage = 3;
+	google.resultsPerPage = 10;
+
+	var resultCount = 3;
 	
 	function CreditsCommand() {
 
@@ -11,21 +13,32 @@ module.exports = function() {
 			params: '$query - search query string',
 			execute: function(data, sandbox) {
 
-				var msg = '\n';
+				var resultsAdded = 0;
 
 				google(data.params, function(err, next, links) {
 
 					if(err) console.error(err);
 
+					var msg = '';
+
 					for(var i = 0; i < links.length; i++) {
 
+						if(resultsAdded >= resultCount) break;
+
 						var link = links[i];
-						msg += link.title + ' - ' + link.link + '\n';
-						if(link.description) msg += link.description + '\n\n';
+
+						if(link.title && link.link) {
+							msg += link.title + ' - ' + link.link + '\n';
+							if(link.description) msg += link.description + '\n\n';
+
+							resultsAdded++
+						}
 					}
 
 
-					sandbox.sendChannelMessage(msg + '\n\n\n');
+					if(msg) {
+						sandbox.sendChannelMessage('\n' + msg + '\n\n\n');
+					}
 
 				});
 
